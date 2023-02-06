90ac5a87477ac8259922ed02832d29e595805bc1
RicardoSantom <richard1978_1@hotmail.com>
2023-02-06 04:33:27 +0100

    Mejora datos carga inicial DB.

--- scriptDB/CargaInicialDB208DWESLoginLogoff.sql
+++ scriptDB/CargaInicialDB208DWESLoginLogoff.sql
@@ -11,17 +11,16 @@ insert into T02_Departamento (T02_CodDepartamento,T02_DescDepartamento,T02_Fecha
                     insert into T02_Departamento  (T02_CodDepartamento,T02_DescDepartamento,T02_FechaCreacionDepartamento,T02_VolumenNegocio)
                     values("EIE","Empresa e Iniciativa Emprendedora",now(),2);
 -- Inserción de datos en la tabla Usuario.
-insert into T01_Usuario(T01_CodUsuario,T01_Password,T01_DescUsuario,T01_FechaHoraUltimaConexion) values
-                    ('heraclio',sha2(concat('heraclio','paso'),256),'Heraclio', now()),
-                    ('alberto',sha2(concat('alberto','paso'),256),'Alberto', now()),
-                    ('amor',sha2(concat('amor','paso'),256),'Amor', now()),
-                    ('antonio',sha2(concat('antonio','paso'),256),'Antonio', now()),
-                    ('carmen',sha2(concat('carmen','paso'),256),'Carmen',now()),
-                    ('ricardo',sha2(concat('ricardo','paso'),256),'Ricardo', now()),
-                    ('david',sha2(concat('david','paso'),256),'David', now()),
-                    ('luis',sha2(concat('luis','paso'),256),'Luis', now()),
-                    ('otalvaro',sha2(concat('otalvaro','paso'),256),'Alejandro', now()),
-                    ('josue',sha2(concat('josue','paso'),256),'Josue', now()),
-                    ('manuel',sha2(concat('manuel','paso'),256),'Manuel', now());
-                             insert into T01_Usuario(T01_CodUsuario,T01_Password,T01_DescUsuario,T01_FechaHoraUltimaConexion,T01_Perfil) values
+insert into T01_Usuario(T01_CodUsuario,T01_Password,T01_DescUsuario,T01_FechaHoraUltimaConexion,T01_Perfil) values
+                    ('heraclio',sha2(concat('heraclio','paso'),256),'Heraclio Borbujo Morán', now(),'profesor'),
+                    ('alberto',sha2(concat('alberto','paso'),256),'Alberto Bahillo Fernández',now(), 'profesor'),
+                    ('amor',sha2(concat('amor','paso'),256),'Amor Rodríguez Navarro',now(), 'profesor'),
+                    ('antonio',sha2(concat('antonio','paso'),256),'Antonio Jañez Veleda',now(), 'profesor'),
+                    ('carmen',sha2(concat('carmen','paso'),256),'Mª del Carmen Fernández Gallego',now(),'profesor'),
+                    ('ricardo',sha2(concat('ricardo','paso'),256),'Ricardo Santiago Tomé',now(), 'alumno'),
+                    ('david',sha2(concat('david','paso'),256),'David Aparicio Sir',now(), 'alumno'),
+                    ('luis',sha2(concat('luis','paso'),256),'Luis Pérez Astorga',now(), 'alumno'),
+                    ('alejandro',sha2(concat('alejandro','paso'),256),'Alejandro Otálvaro Marulanda',now(), 'alumno'),
+                    ('josue',sha2(concat('josue','paso'),256),'Josué Martínez Fernández',now(), 'alumno'),
+                    ('manuel',sha2(concat('manuel','paso'),256),'Manuel Martín Alonso',now(), 'alumno'),
                     ('admin',sha2(concat('admin','paso'),256),'Administrador', now(),'administrador');
